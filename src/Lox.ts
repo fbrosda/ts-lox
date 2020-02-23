51eec774bcad706ea6bebe955e946642e4a0581f
Fabian Brosda <f.brosda@gmx.de>
2020-02-23 14:09:32 +0100

    some basic functionality

--- src/Lox.ts
+++ src/Lox.ts
@@ -1,8 +1,25 @@
+import { promises as fs } from "fs";
+import * as readline from "readline";
 
-export function runScript(path: string): void {
-  console.log("script", path);
+export async function runScript(path: string): Promise<void> {
+  const contents = await fs.readFile(path, { encoding: "utf-8" });
+  run(contents);
 }
 
 export function runPrompt(): void {
-  console.log("prompt");
+  const rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt("> ");
+  rl.on("line", processLine);
+  rl.on("close", () => process.exit(0));
+
+  rl.prompt();
+
+  function processLine(line: string): void {
+    run(line);
+    rl.prompt();
+  }
+}
+
+function run(source: string): void {
+  console.log(source);
 }
