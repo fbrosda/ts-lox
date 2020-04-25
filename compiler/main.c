982ebb98203e82f1384293728b792fdb49aa27c5
Fabian Brosda <f.brosda@gmx.de>
2020-04-25 15:00:11 +0200

    simplistic c setup

--- compiler/main.c
+++ compiler/main.c
@@ -1,6 +1,13 @@
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
-int main(int argc, char const* argv[])
-{
+int main() {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
